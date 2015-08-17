0d64071a946aedb9d48ef343e84172688e078190
aullik <nicolaswehrle@gmx.net>
2015-08-17 18:13:14 +0200

    Export classes into new files

---
+++ src/de/blenderOpenGEX/BlenderOpenGEX.py
@@ -0,0 +1,75 @@
+__author__ = 'aullik'
+
+
+# =============================================================
+#
+#  Open Game Engine Exchange
+#  http://opengex.org/
+#
+#  Export plugin for Blender
+#  by Eric Lengyel
+#
+#  Version 1.1.2.3
+#
+#  Copyright 2015, Terathon Software LLC
+#
+#  This software is licensed under the Creative Commons
+#  Attribution-ShareAlike 3.0 Unported License:
+#
+#  http://creativecommons.org/licenses/by-sa/3.0/deed.en_US
+#
+# =============================================================
+
+
+bl_info = {
+	"name": "OpenGEX format (.ogex)",
+	"description": "Terathon Software OpenGEX Exporter",
+	"author": "Eric Lengyel",
+	"version": (1, 1, 2, 3),
+	"location": "File > Import-Export",
+	"wiki_url": "http://opengex.org/",
+	"category": "Import-Export"}
+
+
+import bpy
+from blenderOpenGEX.MyOpenGexExporter import OpenGexExporter
+
+
+kNodeTypeNode = 0
+kNodeTypeBone = 1
+kNodeTypeGeometry = 2
+kNodeTypeLight = 3
+kNodeTypeCamera = 4
+
+kAnimationSampled = 0
+kAnimationLinear = 1
+kAnimationBezier = 2
+
+kExportEpsilon = 1.0e-6
+
+
+structIdentifier = [B"Node $", B"BoneNode $", B"GeometryNode $", B"LightNode $", B"CameraNode $"]
+
+
+subtranslationName = [B"xpos", B"ypos", B"zpos"]
+subrotationName = [B"xrot", B"yrot", B"zrot"]
+subscaleName = [B"xscl", B"yscl", B"zscl"]
+deltaSubtranslationName = [B"dxpos", B"dypos", B"dzpos"]
+deltaSubrotationName = [B"dxrot", B"dyrot", B"dzrot"]
+deltaSubscaleName = [B"dxscl", B"dyscl", B"dzscl"]
+axisName = [B"x", B"y", B"z"]
+
+
+def menu_func(self, context):
+	self.layout.operator(OpenGexExporter.bl_idname, text = "OpenGEX (.ogex)")
+
+def register():
+	bpy.utils.register_class(OpenGexExporter)
+	bpy.types.INFO_MT_file_export.append(menu_func)
+
+def unregister():
+	bpy.types.INFO_MT_file_export.remove(menu_func)
+	bpy.utils.unregister_class(OpenGexExporter)
+
+if __name__ == "__main__":
+	register()
---
+++ src/de/blenderOpenGEX/ExportVertex.py
@@ -0,0 +1,41 @@
+__author__ = 'aullik'
+
+
+class ExportVertex:
+	__slots__ = ("hash", "vertexIndex", "faceIndex", "position", "normal", "color", "texcoord0", "texcoord1")
+
+	def __init__(self):
+		self.color = [1.0, 1.0, 1.0]
+		self.texcoord0 = [0.0, 0.0]
+		self.texcoord1 = [0.0, 0.0]
+
+	def __eq__(self, v):
+		if (self.hash != v.hash):
+			return (False)
+		if (self.position != v.position):
+			return (False)
+		if (self.normal != v.normal):
+			return (False)
+		if (self.color != v.color):
+			return (False)
+		if (self.texcoord0 != v.texcoord0):
+			return (False)
+		if (self.texcoord1 != v.texcoord1):
+			return (False)
+		return (True)
+
+	def Hash(self):
+		h = hash(self.position[0])
+		h = h * 21737 + hash(self.position[1])
+		h = h * 21737 + hash(self.position[2])
+		h = h * 21737 + hash(self.normal[0])
+		h = h * 21737 + hash(self.normal[1])
+		h = h * 21737 + hash(self.normal[2])
+		h = h * 21737 + hash(self.color[0])
+		h = h * 21737 + hash(self.color[1])
+		h = h * 21737 + hash(self.color[2])
+		h = h * 21737 + hash(self.texcoord0[0])
+		h = h * 21737 + hash(self.texcoord0[1])
+		h = h * 21737 + hash(self.texcoord1[0])
+		h = h * 21737 + hash(self.texcoord1[1])
+		self.hash = h
