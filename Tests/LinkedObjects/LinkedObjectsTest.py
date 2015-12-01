adbf9b65d8c70b0b20c645c325f30b6f044d60ad
Squareys <Squareys@googlemail.com>
2015-12-01 19:45:59 +0100

    Cleanup LinkedObjectsTest

--- Tests/LinkedObjects/LinkedObjectsTest.py
+++ Tests/LinkedObjects/LinkedObjectsTest.py
@@ -8,14 +8,16 @@ from blenderOpenGEX.MyOpenGexExporter import *
 
 
 class LinkedObjectsTest(unittest.TestCase):
-    def test_something(self):
+
+    def setUp(self):
         blenderOpenGEX.register()
-        base_dir = os.path.dirname(os.path.realpath(__file__))
+        self.base_dir = os.path.dirname(os.path.realpath(__file__))
 
-        bpy.ops.wm.open_mainfile(filepath=base_dir + "\\Test.blend")
-        bpy.ops.export_scene.ogex(filepath=base_dir + "\\Test.ogex")
+    def testLinkedObjects(self):
+        bpy.ops.wm.open_mainfile(filepath=self.base_dir + "\\Test.blend")
+        bpy.ops.export_scene.ogex(filepath=self.base_dir + "\\Test.ogex")
 
-        self.assertEqual(open(base_dir + "\\Test.ogex").read(), open(base_dir + "\\Expected.ogex").read())
+        self.assertEqual(open(self.base_dir + "\\Test.ogex").read(), open(self.base_dir + "\\Expected.ogex").read())
 
 if __name__ == '__main__':
     unittest.main()
