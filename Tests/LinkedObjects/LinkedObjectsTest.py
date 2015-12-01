b324f30b11aed92567945cf8929e4173128d3e9b
Squareys <Squareys@googlemail.com>
2015-12-01 20:09:05 +0100

    Delete test output file on tearDown
    And remove some more obsolete files.
    
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- Tests/LinkedObjects/LinkedObjectsTest.py
+++ Tests/LinkedObjects/LinkedObjectsTest.py
@@ -9,13 +9,20 @@ from blenderOpenGEX.MyOpenGexExporter import *
 
 class LinkedObjectsTest(unittest.TestCase):
 
+    # name of the output file
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    filename = base_dir + "\\Test.ogex"
+
     def setUp(self):
         blenderOpenGEX.register()
-        self.base_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def tearDown(self):
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
 
     def testLinkedObjects(self):
         bpy.ops.wm.open_mainfile(filepath=self.base_dir + "\\Test.blend")
-        bpy.ops.export_scene.ogex(filepath=self.base_dir + "\\Test.ogex")
+        bpy.ops.export_scene.ogex(filepath=self.filename)
 
         self.assertEqual(open(self.base_dir + "\\Test.ogex").read(), open(self.base_dir + "\\Expected.ogex").read())
 
