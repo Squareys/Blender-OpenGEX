0519447d372573d725372bfc4f8ceec263b72d9f
Squareys <Squareys@googlemail.com>
2015-12-01 23:21:10 +0100

    Enhance handling instance group offset

--- src/org/ogex/blender/OpenGexExporter.py
+++ src/org/ogex/blender/OpenGexExporter.py
@@ -1086,13 +1086,9 @@ class OpenGexExporter(bpy.types.Operator, ExportHelper, Writer):
             self.write_matrix(matrix)
             return
 
-        line0 = (matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3] - offset[0])
-        line1 = (matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3] - offset[1])
-        line2 = (matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3] - offset[2])
-        line3 = (matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3])
-
-        matrix = (line0, line1, line2, line3)
-        self.write_matrix(matrix)
+        m = matrix.copy()
+        m.translation -= offset
+        self.write_matrix(m)
 
     def export_bone_transform(self, nw, bw, scene):  # armature, bone, scene):
 
