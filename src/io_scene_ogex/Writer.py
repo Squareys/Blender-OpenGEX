323e7353c5fb4dadfb44d68970606fe9429910ef
Squareys <Squareys@googlemail.com>
2015-12-02 22:35:40 +0100

    Some code optimization tricks
    Less write calls, writing more at a time.

--- src/io_scene_ogex/Writer.py
+++ src/io_scene_ogex/Writer.py
@@ -1,4 +1,5 @@
 import math
+from itertools import zip_longest
 
 __author__ = 'Eric Lengyel, Jonathan Hale, Nicolas Wehrle'
 
@@ -10,25 +11,25 @@ class Writer:
         self.file = None
         self.indentLevel = 0
         pass
-     
+
     def open(self, filename):
         self.file = open(filename, "wb")
-        
+
     def close(self):
         self.file.close()
-     
+
     def set_file(self, file):
         self.file = file
-        
+
     def write(self, data):
         self.file.write(data)  # TODO inline later!
-        
+
     def inc_indent(self):
         self.indentLevel += 1
-     
+
     def dec_indent(self):
         self.indentLevel -= 1
-        
+
     def indent_write(self, text, extra=0, newline=False):
         if newline:
             self.file.write(B"\n")
@@ -37,7 +38,7 @@ class Writer:
 
     def get_indent(self, extra=0):
         return B"\t"*(self.indentLevel + extra)
-        
+
     def write_int(self, i):
         self.file.write(bytes(str(i), "UTF-8"))
 
@@ -48,19 +49,22 @@ class Writer:
         else:
             # TODO make float rounding optional
             return bytes(str(round(f, 6)), "UTF-8")
-            
+
+    @staticmethod
+    def to_int_byte(i):
+        return bytes(str(i), "UTF-8")
+
     def write_float(self, f):
         self.file.write(self.to_float_byte(f))
-     
+
     def write_matrix_row(self, matrix, i):
-        self.write_float(matrix[0][i])
-        self.file.write(B", ")
-        self.write_float(matrix[1][i])
-        self.file.write(B", ")
-        self.write_float(matrix[2][i])
-        self.file.write(B", ")
-        self.write_float(matrix[3][i])
-        
+        to_f = Writer.to_float_byte
+        self.write(
+            to_f(matrix[0][i]) + B", " +
+            to_f(matrix[1][i]) + B", " +
+            to_f(matrix[2][i]) + B", " +
+            to_f(matrix[3][i]))
+
     def write_matrix(self, matrix):
         self.inc_indent()
         self.indent_write(B"{")
@@ -73,7 +77,7 @@ class Writer:
         self.write_matrix_row(matrix, 3)
         self.file.write(B"}\n")
         self.dec_indent()
-        
+
     def write_matrix_flat(self, matrix):
         self.indent_write(B"{", 1)
         self.write_matrix_row(matrix, 0)
@@ -84,7 +88,7 @@ class Writer:
         self.file.write(B", ")
         self.write_matrix_row(matrix, 3)
         self.file.write(B"}")
-        
+
     def write_color(self, color):
         self.file.write(B"{")
         self.write_float(color[0])
@@ -93,7 +97,7 @@ class Writer:
         self.file.write(B", ")
         self.write_float(color[2])
         self.file.write(B"}")
-        
+
     def write_file_name(self, filename):
         length = len(filename)
         if (length > 2) and (filename[1] == ":"):
@@ -102,21 +106,16 @@ class Writer:
             self.file.write(bytes(filename[2:length].replace("\\", "/"), "UTF-8"))
         elif length != 0:
             self.file.write(bytes(filename.replace("\\", "/"), "UTF-8"))
-                
+
     def write_int_array(self, value_array):
+        to_i = self.to_int_byte
         count = len(value_array)
         k = 0
 
         line_count = count >> 6
         for i in range(line_count):
-            self.indent_write(B"", 1)
-            for j in range(63):
-                self.write_int(value_array[k])
-                self.file.write(B", ")
-                k += 1
-
-            self.write_int(value_array[k])
-            k += 1
+            self.indent_write(B"" + (B", ".join(map(to_i, value_array[k:k+64]))), 1)
+            k += 64
 
             if i * 64 < count - 64:
                 self.file.write(B",\n")
@@ -133,8 +132,9 @@ class Writer:
 
             self.write_int(value_array[k])
             self.file.write(B"\n")
-            
+
     def write_float_array(self, value_array):
+        to_f = Writer.to_float_byte
         count = len(value_array)
         k = 0
 
@@ -142,8 +142,7 @@ class Writer:
         for i in range(line_count):
             self.indent_write(B"", 1)
             for j in range(15):
-                self.write_float(value_array[k])
-                self.file.write(B", ")
+                self.file.write(to_f(value_array[k]) + B", ")
                 k += 1
 
             self.write_float(value_array[k])
@@ -158,50 +157,34 @@ class Writer:
         if count != 0:
             self.indent_write(B"", 1)
             for j in range(count - 1):
-                self.write_float(value_array[k])
-                self.file.write(B", ")
+                self.file.write(to_f(value_array[k]) + B", ")
                 k += 1
 
             self.write_float(value_array[k])
             self.file.write(B"\n")
 
     def write_vector2d(self, vector):
-        self.file.write(B"{")
-        self.write_float(vector[0])
-        self.file.write(B", ")
-        self.write_float(vector[1])
-        self.file.write(B"}")
+        to_f = Writer.to_float_byte
+        self.file.write(B"{" + to_f(vector[0]) + B", " +
+                        to_f(vector[1]) + B"}")
 
     def write_vector3d(self, vector):
-        self.file.write(B"{")
-        self.write_float(vector[0])
-        self.file.write(B", ")
-        self.write_float(vector[1])
-        self.file.write(B", ")
-        self.write_float(vector[2])
-        self.file.write(B"}")
+        to_f = Writer.to_float_byte
+        self.file.write(B"{" +
+                        to_f(vector[0]) + B", " +
+                        to_f(vector[1]) + B", " +
+                        to_f(vector[2]) + B"}")
 
     def write_vector4d(self, vector):
-        self.file.write(B"{")
-        self.write_float(vector[0])
-        self.file.write(B", ")
-        self.write_float(vector[1])
-        self.file.write(B", ")
-        self.write_float(vector[2])
-        self.file.write(B", ")
-        self.write_float(vector[3])
-        self.file.write(B"}")
+        to_f = Writer.to_float_byte
+        self.file.write(B"{" +
+                        to_f(vector[0]) + B", " +
+                        to_f(vector[1]) + B", " +
+                        to_f(vector[2]) + B", " +
+                        to_f(vector[3]) + B"}")
 
     def write_quaternion(self, quaternion):
-        self.file.write(B"{")
-        self.write_float(quaternion[1])
-        self.file.write(B", ")
-        self.write_float(quaternion[2])
-        self.file.write(B", ")
-        self.write_float(quaternion[3])
-        self.file.write(B", ")
-        self.write_float(quaternion[0])
-        self.file.write(B"}")
+        self.write_vector4d(quaternion)
 
     def write_vertex_array2d(self, vertex_array, attrib):
         count = len(vertex_array)
@@ -333,31 +316,26 @@ class Writer:
             self.write_vector3d(mesh_vertex_array[vertex_array[k].vertexIndex].normal if face.use_smooth else face.normal)
             self.file.write(B"\n")
 
-    def write_triangle(self, triangleIndex, indexTable):
-        i = triangleIndex * 3
-        self.file.write(B"{")
-        self.write_int(indexTable[i])
-        self.file.write(B", ")
-        self.write_int(indexTable[i + 1])
-        self.file.write(B", ")
-        self.write_int(indexTable[i + 2])
-        self.file.write(B"}")
+    @staticmethod
+    def grouper(iterable, n):
+        "Collect data into fixed-length chunks or blocks"
+        # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+        args = [iter(iterable)] * n
+        return zip_longest(*args, fillvalue="")
 
-    def write_triangle_array(self, count, indexTable):
-        triangle_index = 0
+    def write_triangle_array(self, count, index_table):
+        to_i = self.to_int_byte
+        i = 0  # index of fist index of current triangle
 
         line_count = count >> 4
-        for i in range(line_count):
+        for x in range(line_count):
             self.indent_write(B"", 1)
-            for j in range(15):
-                self.write_triangle(triangle_index, indexTable)
-                self.file.write(B", ")
-                triangle_index += 1
 
-            self.write_triangle(triangle_index, indexTable)
-            triangle_index += 1
+            self.write(B", ".join([(B"{" + (B", ".join(map(to_i, tri))) + B"}")
+                       for tri in self.grouper(index_table[i:i + 16*3], 3)]))
+            i += 16*3
 
-            if i * 16 < count - 16:
+            if x * 16 < count - 16:
                 self.file.write(B",\n")
             else:
                 self.file.write(B"\n")
@@ -365,13 +343,8 @@ class Writer:
         count &= 15
         if count != 0:
             self.indent_write(B"", 1)
-            for j in range(count - 1):
-                self.write_triangle(triangle_index, indexTable)
-                self.file.write(B", ")
-                triangle_index += 1
-
-            self.write_triangle(triangle_index, indexTable)
-            self.file.write(B"\n")
+            self.write(B", ".join([(B"{" + (B", ".join(map(to_i, tri))) + B"}")
+                       for tri in self.grouper(index_table[i:i + count*3], 3)]) + B"\n")
 
     def write_node_table(self, objectRef):
         first = True
