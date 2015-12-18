d34f932a9a7b92fe7a408b3806ce92710df4b813
Squareys <Squareys@googlemail.com>
2015-12-18 17:57:35 +0100

    Add test for physics properties export
    Coming actual functionality coming later. I guess you could call this test-
    driven development.
    
    Signed-off-by: Squareys <Squareys@googlemail.com>

---
+++ tests/Physics/PhysicsTest.py
@@ -0,0 +1,25 @@
+import bpy
+import os
+import unittest
+
+import io_scene_ogex
+
+from tests import TestUtils
+
+__author__ = 'Jonathan Hale'
+
+
+class PhsyicsTest(TestUtils.OgexExporterTest):
+
+    # name of the output file
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    filename = base_dir + os.sep + "Test.ogex"
+
+    def testLinkedObjects(self):
+        bpy.ops.wm.open_mainfile(filepath=self.base_dir + os.sep + "Test.blend")
+        bpy.ops.export_scene.ogex(filepath=self.filename)
+
+        self.assertFilesEqual(self.filename, self.base_dir + os.sep + "Expected.ogex")
+
+if __name__ == '__main__':
+    unittest.main()
