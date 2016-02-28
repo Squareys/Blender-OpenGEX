f64546ff91859c1beda13dc373c28159bde9e2fc
Squareys <Squareys@googlemail.com>
2016-02-29 00:03:25 +0100

    Add some comments to run_tests.py
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,10 @@
 import unittest
+
+__author__ = "Jonathan Hale"
+
+# find all unittests in tests/
 suite = unittest.TestLoader().discover('.', pattern='*Test.py', top_level_dir='.')
+
+# run the found tests and exit with failure when not successful
 if not unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful():
     exit(1)
