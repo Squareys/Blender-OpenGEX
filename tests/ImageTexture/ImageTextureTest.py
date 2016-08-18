909f61a316e64c7a11230385428e700e6fde4539
Squareys <Squareys@googlemail.com>
2016-08-18 17:28:49 +0200

    tests: Also test image conversion when exporting image texture
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- tests/ImageTexture/ImageTextureTest.py
+++ tests/ImageTexture/ImageTextureTest.py
@@ -18,10 +18,10 @@ class ImageTextureTest(TestUtils.OgexExporterTest):
         bpy.ops.wm.open_mainfile(filepath=self.base_dir + os.sep + "Test.blend")
         bpy.ops.export_scene.ogex(filepath=self.filename, rounding=3,
                                   export_image_textures=True, image_path_prefix="//textures/",
-                                  image_format='PNG')
+                                  image_format='BMP')
 
         self.assertFilesEqual(self.filename, self.base_dir + os.sep + "Expected.ogex")
-        self.assertTrue(os.path.isfile(os.path.join(self.base_dir, "textures", "Test.png")))
+        self.assertTrue(os.path.isfile(os.path.join(self.base_dir, "textures", "Test.bmp")))
 
 if __name__ == '__main__':
     unittest.main()
