3253999d1fd846e80acc9d63c45a9e79e26c9da8
Squareys <Squareys@googlemail.com>
2016-08-18 15:02:40 +0200

    tests: Add ImageTextureTest
    Signed-off-by: Squareys <Squareys@googlemail.com>

---
+++ tests/ImageTexture/ImageTextureTest.py
@@ -0,0 +1,27 @@
+import bpy
+import os
+import unittest
+
+from tests import TestUtils
+
+__author__ = 'Jonathan Hale'
+
+
+# Regression test for exporting image textures
+class ImageTextureTest(TestUtils.OgexExporterTest):
+
+    # name of the output file
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    filename = base_dir + os.sep + "Test.ogex"
+
+    def testImageTextureExport(self):
+        bpy.ops.wm.open_mainfile(filepath=self.base_dir + os.sep + "Test.blend")
+        bpy.ops.export_scene.ogex(filepath=self.filename, rounding=3,
+                                  export_image_textures=True, image_path_prefix="//textures/",
+                                  image_format='PNG')
+
+        self.assertFilesEqual(self.filename, self.base_dir + os.sep + "Expected.ogex")
+        self.assertTrue(os.path.isfile(os.path.join(self.base_dir, "textures", "Test.png")))
+
+if __name__ == '__main__':
+    unittest.main()
