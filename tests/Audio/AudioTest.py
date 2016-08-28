9d8632f9c48037788654616d4118bfa512fbf77f
Squareys <Squareys@googlemail.com>
2016-08-28 14:35:51 +0200

    Add test for Audio export
    Signed-off-by: Squareys <Squareys@googlemail.com>

---
+++ tests/Audio/AudioTest.py
@@ -0,0 +1,23 @@
+import bpy
+import os
+import unittest
+
+from tests import TestUtils
+
+__author__ = 'Jonathan Hale'
+
+
+class AudioTest(TestUtils.OgexExporterTest):
+
+    # name of the output file
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    filename = base_dir + os.sep + "Test.ogex"
+
+    def testAudio(self):
+        bpy.ops.wm.open_mainfile(filepath=self.base_dir + os.sep + "Test.blend")
+        bpy.ops.export_scene.ogex(filepath=self.filename, export_audio=True, rounding=3, audio_path_prefix="audio/")
+
+        self.assertFilesEqual(self.filename, self.base_dir + os.sep + "Expected.ogex")
+
+if __name__ == '__main__':
+    unittest.main()
