c8fb7ce7c5606ae8dcfa6e3ed5b0d7b8e6f046f0
Squareys <Squareys@googlemail.com>
2016-09-21 17:03:15 +0200

    Add tests for more physics properties
    Feature implemented in the next commit.
    
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- tests/Physics/PhysicsTest.py
+++ tests/Physics/PhysicsTest.py
@@ -7,17 +7,24 @@ from tests import TestUtils
 __author__ = 'Jonathan Hale'
 
 
-class PhsyicsTest(TestUtils.OgexExporterTest):
+class PhysicsTest(TestUtils.OgexExporterTest):
 
     # name of the output file
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    filename = base_dir + os.sep + "Test.ogex"
 
-    def testPhysics(self):
-        bpy.ops.wm.open_mainfile(filepath=self.base_dir + os.sep + "Test.blend")
+    def testBasicProperties(self):
+        self.filename = self.base_dir + os.sep + "BasicProperties.ogex"
+        bpy.ops.wm.open_mainfile(filepath=self.base_dir + os.sep + "BasicProperties.blend")
         bpy.ops.export_scene.ogex(filepath=self.filename, export_physics=True, rounding=3)
 
-        self.assertFilesEqual(self.filename, self.base_dir + os.sep + "Expected.ogex")
+        self.assertFilesEqual(self.filename, self.base_dir + os.sep + "BasicPropertiesExpected.ogex")
+
+    def testAdvancedProperties(self):
+        self.filename = self.base_dir + os.sep + "AdvancedProperties.ogex"
+        bpy.ops.wm.open_mainfile(filepath=self.base_dir + os.sep + "AdvancedProperties.blend")
+        bpy.ops.export_scene.ogex(filepath=self.filename, export_physics=True, rounding=3)
+
+        self.assertFilesEqual(self.filename, self.base_dir + os.sep + "AdvancedPropertiesExpected.ogex")
 
 if __name__ == '__main__':
     unittest.main()
